a2cf2d133a3ecebcccad6646bb681286044f8bf0
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-06 16:20:14 +0900

    utils.IsLowerUUID() を実装

--- backend/src/utils/uuid.go
+++ backend/src/utils/uuid.go
@@ -1,9 +1,15 @@
 package utils
 
 import (
+	"regexp"
+
 	"github.com/google/uuid"
 )
 
 func UUIDV4() string {
 	return uuid.New().String()
+}
+
+func IsLowerUUID(s string) bool {
+	return regexp.MustCompile(`\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z`).Match([]byte(s))
 }
--- backend/src/utils/uuid_test.go
+++ backend/src/utils/uuid_test.go
@@ -13,3 +13,22 @@ func Test_UUIDV4(t *testing.T) {
 		assert.Regexp(t, regexp.MustCompile(`\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z`), u)
 	})
 }
+
+func Test_IsLowerUUID(t *testing.T) {
+	t.Run("return is string uuid", func(t *testing.T) {
+		testcases := []struct {
+			str string
+			ok  bool
+		}{
+			{"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa", true},
+			{"00000000-0000-0000-0000-000000000000", true},
+			{"da078009-a2b4-4e6d-9004-cb0096ab9f62", true},
+			{"AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA", false},
+			{"DA078009-A2B4-4E6D-9004-CB0096AB9F62", false},
+		}
+
+		for _, testcase := range testcases {
+			assert.Equal(t, testcase.ok, IsLowerUUID(testcase.str))
+		}
+	})
+}
