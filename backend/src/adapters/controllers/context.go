b7eb5948e3b10f257d61d0f30f179653ad75f461
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-07 20:25:31 +0900

    GetRequestID を実装

--- backend/src/adapters/controllers/context.go
+++ backend/src/adapters/controllers/context.go
@@ -4,4 +4,5 @@ type Context interface {
 	JSON(code int, obj interface{})
 	ShouldBindQuery(obj interface{}) error
 	ShouldBindJSON(obj interface{}) error
+	GetRequestID() string
 }
--- backend/src/infrastructures/gin_context.go
+++ backend/src/infrastructures/gin_context.go
@@ -1,6 +1,10 @@
 package infrastructures
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/awslabs/aws-lambda-go-api-proxy/core"
+	"github.com/gin-gonic/gin"
+)
 
 type GinContext struct {
 	*gin.Context
@@ -10,4 +14,19 @@ func NewContextFromGin(ctx *gin.Context) *GinContext {
 	return &GinContext{
 		Context: ctx,
 	}
+}
+
+func (ctx *GinContext) GetAPIGatewayProxyRequestContext() (events.APIGatewayProxyRequestContext, bool) {
+	return core.GetAPIGatewayContextFromContext(ctx.Context)
+}
+
+func (ctx *GinContext) GetRequestID() string {
+	apigwctx, ok := ctx.GetAPIGatewayProxyRequestContext()
+	if !ok {
+		panic("api gateway proxy request context not found")
+	}
+	if apigwctx.RequestID == "" {
+		panic("request id not found")
+	}
+	return apigwctx.RequestID
 }
