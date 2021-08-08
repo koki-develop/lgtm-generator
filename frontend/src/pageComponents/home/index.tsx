efb586198e0c3d3aad3c450eba61fe967bfaea14
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-08 23:25:06 +0900

    Home ページコンポーネントを作成

---
+++ frontend/src/pageComponents/home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home: React.VFC = () => {
+  return (
+    <div>hello world</div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,3 @@
+import Home from '../pageComponents/home';
+
+export default Home;
