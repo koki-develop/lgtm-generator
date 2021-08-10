9f485d5e1b9bf349ba8ca9093c87f54616473160
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-10 20:43:36 +0900

    不要なメソッドを削除

--- backend/src/adapters/controllers/context.go
+++ backend/src/adapters/controllers/context.go
@@ -4,5 +4,4 @@ type Context interface {
 	JSON(code int, obj interface{})
 	ShouldBindQuery(obj interface{}) error
 	ShouldBindJSON(obj interface{}) error
-	GetRequestID() string
 }
--- backend/src/infrastructures/gin_context.go
+++ backend/src/infrastructures/gin_context.go
@@ -1,8 +1,6 @@
 package infrastructures
 
 import (
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/awslabs/aws-lambda-go-api-proxy/core"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,19 +12,4 @@ func NewContextFromGin(ctx *gin.Context) *GinContext {
 	return &GinContext{
 		Context: ctx,
 	}
-}
-
-func (ctx *GinContext) GetAPIGatewayProxyRequestContext() (events.APIGatewayProxyRequestContext, bool) {
-	return core.GetAPIGatewayContextFromContext(ctx.Context.Request.Context())
-}
-
-func (ctx *GinContext) GetRequestID() string {
-	apigwctx, ok := ctx.GetAPIGatewayProxyRequestContext()
-	if !ok {
-		panic("api gateway proxy request context not found")
-	}
-	if apigwctx.RequestID == "" {
-		panic("request id not found")
-	}
-	return apigwctx.RequestID
 }
