d08640973035467116ec37a02e8385a57b1dcd65
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-10 20:45:09 +0900

    Context に Status() を追加

--- backend/src/adapters/controllers/context.go
+++ backend/src/adapters/controllers/context.go
@@ -3,6 +3,7 @@ package controllers
 type Context interface {
 	Abort()
 	JSON(code int, obj interface{})
+	Status(code int)
 	ShouldBindQuery(obj interface{}) error
 	ShouldBindJSON(obj interface{}) error
 }
