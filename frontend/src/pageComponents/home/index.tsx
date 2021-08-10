7167b039e8a36f0c64f6e5ac2350d33df0592be3
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-11 08:44:08 +0900

    とりあえず画像表示まで

--- frontend/src/pageComponents/home/index.tsx
+++ frontend/src/pageComponents/home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Lgtm } from '../../types/lgtm';
 import { ApiClient } from '../../lib/apiClient';
+import urlJoin from 'url-join';
 
 const Home: React.VFC = () => {
   const [lgtms, setLgtms] = useState<Lgtm[]>([]);
@@ -13,7 +14,10 @@ const Home: React.VFC = () => {
     <div>
       {lgtms.map(lgtm => (
         <div key={lgtm.id}>
-          <p>{lgtm.id}</p>
+          <img
+            src={`${urlJoin(process.env.NEXT_PUBLIC_LGTMS_ORIGIN, lgtm.id)}`}
+            style={{ width: 300 }}
+          />
         </div>
       ))}
     </div>
