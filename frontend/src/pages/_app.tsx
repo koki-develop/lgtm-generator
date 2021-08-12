1672ed35324c8dbac0ac455b6be1c54ed2c2c8ff
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-13 08:30:37 +0900

    フォントを読み込み

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import '../styles/global.scss';
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
