380b82c5af15b8e12b8ec1ddeb2ea6424a951163
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-13 15:56:43 +0900

    Field コンポーネントを作成

---
+++ frontend/src/components/field/index.tsx
@@ -0,0 +1,12 @@
+import {
+  Box,
+} from '@material-ui/core';
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+export default withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginBottom: theme.spacing(2),
+    },
+  }),
+)(Box);
