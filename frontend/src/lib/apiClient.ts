f2fa3b7edbce2c2553ce9447476c3fb9c2684cdb
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-08-14 09:00:12 +0900

    ApiClient.createLgtm() を実装

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -11,13 +11,24 @@ export class ApiClient {
   public static async getLgtms(after?: string): Promise<Lgtm[]> {
     const endpoint = this.buildEndpoint('v1', 'lgtms');
     const response = await axios.get<LgtmRaw[]>(endpoint, { params: { after } });
-    return response.data.map(raw => ({
-      id: raw.id,
-      createdAt: new Date(raw.created_at),
-    }));
+    return response.data.map(this.lgtmFromRaw);
+  }
+
+  public static async createLgtm(base64: string, contentType: string): Promise<Lgtm> {
+    const endpoint = this.buildEndpoint('v1', 'lgtms');
+    const body = { base64, content_type: contentType };
+    const response = await axios.post<LgtmRaw>(endpoint, body);
+    return this.lgtmFromRaw(response.data);
   }
 
   private static buildEndpoint(...paths: string[]): string {
     return urlJoin(process.env.NEXT_PUBLIC_API_ORIGIN, ...paths);
+  }
+
+  private static lgtmFromRaw(raw: LgtmRaw): Lgtm {
+    return {
+      id: raw.id,
+      createdAt: new Date(raw.created_at),
+    };
   }
 }
