d18b850b2dba0f33ccf0dac031a0badd30c33d8a
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-09-04 08:30:12 +0900

    ページング機能を実装

--- frontend/src/pageComponents/home/lgtmsPanel/index.tsx
+++ frontend/src/pageComponents/home/lgtmsPanel/index.tsx
@@ -6,23 +6,46 @@ import { DataUrl } from '~/lib/dataUrl';
 import { useToast } from '~/contexts/toastProvider';
 import {
   Box,
+  Button,
   Grid,
 } from '@material-ui/core';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+} from '@material-ui/core/styles';
 import UploadButton from './uploadButton';
 import LgtmCard from './lgtmCard';
 import ConfirmForm from '../confirmForm';
 import Loading from '~/components/loading';
 import Modal from '~/components/modal';
 
-const perPage = 20;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    loadingContainer: {
+      marginTop: theme.spacing(2),
+    },
+    moreButtonContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
+
+const perPage = 2;
 
 const LgtmsPanel: React.VFC = React.memo(() => {
+  const classes = useStyles();
+
   const [lgtms, setLgtms] = useState<Lgtm[]>([]);
   const [uploading, setUploading] = useState<boolean>(false);
   const [loadingImage, setLoadingImage] = useState<boolean>(false);
   const [openConfirmForm, setOpenConfirmForm] = useState<boolean>(false);
   const [previewDataUrl, setPreviewDataUrl] = useState<string>();
   const [previewContentType, setPreviewContentType] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [showMore, setShowMore] = useState<boolean>(false);
   const { enqueueSuccess } = useToast();
 
   const handleCloseConfirmForm = () => {
@@ -41,17 +64,29 @@ const LgtmsPanel: React.VFC = React.memo(() => {
 
   const handleConfirm = () => {
     setUploading(true);
-    ApiClient.createLgtm(new DataUrl(previewDataUrl).toBase64(), previewContentType).then(() => {
+    ApiClient.createLgtm(new DataUrl(previewDataUrl).toBase64(), previewContentType).then(lgtm => {
       setUploading(false);
       setOpenConfirmForm(false);
+      setLgtms(prev => [lgtm, ...prev]);
       enqueueSuccess('LGTM 画像を生成しました');
     });
   };
 
-  useEffect(() => {
-    ApiClient.getLgtms(perPage).then(lgtms => {
-      setLgtms(lgtms);
+  const handleClickMore = () => {
+    loadLgtms();
+  };
+
+  const loadLgtms = () => {
+    setLoading(true);
+    ApiClient.getLgtms(perPage, lgtms.slice(-1)[0]?.id).then(lgtms => {
+      setLgtms(prev => [...prev, ...lgtms]);
+      setShowMore(lgtms.length === perPage);
+      setLoading(false);
     });
+  };
+
+  useEffect(() => {
+    loadLgtms();
   }, []);
 
   return (
@@ -83,7 +118,20 @@ const LgtmsPanel: React.VFC = React.memo(() => {
             />
           </Grid>
         ))}
-        </Grid>
+      </Grid>
+
+      <Box className={classes.moreButtonContainer}>
+        {loading && <Loading />}
+        {!loading && showMore && (
+            <Button
+              color='primary'
+              variant='contained'
+              onClick={handleClickMore}
+            >
+              もっと見る
+            </Button>
+        )}
+      </Box>
     </Box>
   );
 });
