f206ba5c9e53feb00cb5f442adb2fb9afa45e12a
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-09-11 16:44:55 +0900

    お気に入りがない場合にメッセージを表示

--- frontend/src/pageComponents/home/favoritesPanel/index.tsx
+++ frontend/src/pageComponents/home/favoritesPanel/index.tsx
@@ -1,14 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { favoriteIdsState } from '~/recoil/atoms';
-import { Box } from '@material-ui/core';
+import {
+  Box,
+  Typography,
+} from '@material-ui/core';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+} from '@material-ui/core/styles';
 import LgtmCardList from '../lgtmCardList';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    emptyMessage: {
+      color: theme.palette.text.secondary,
+      textAlign: 'center',
+    },
+  }),
+);
 
 type FavoritesPanelProps = {
   show: boolean;
 };
 
 const FavoritesPanel: React.VFC<FavoritesPanelProps> = React.memo((props: FavoritesPanelProps) => {
+  const classes = useStyles();
+
   const favoriteIds = useRecoilValue(favoriteIdsState);
   const [showingFavoriteIds, setShowingFavoriteIds] = useState<string[]>([]);
 
@@ -18,7 +37,11 @@ const FavoritesPanel: React.VFC<FavoritesPanelProps> = React.memo((props: Favori
 
   return (
     <Box hidden={!props.show}>
-      <LgtmCardList ids={showingFavoriteIds} />
+      {showingFavoriteIds.length === 0 ? (
+        <Typography className={classes.emptyMessage}>お気に入りした LGTM 画像はありません。</Typography>
+      ) : (
+        <LgtmCardList ids={showingFavoriteIds} />
+      )}
     </Box>
   );
 });
