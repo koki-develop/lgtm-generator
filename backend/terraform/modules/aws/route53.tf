f597a13149ec70ec3fcbb112129ef3074af11096
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-09-11 09:59:08 +0900

    api, lgtm 画像用のレコードを発行

--- backend/terraform/modules/aws/route53.tf
+++ backend/terraform/modules/aws/route53.tf
@@ -3,12 +3,36 @@ data "aws_route53_zone" "default" {
   private_zone = false
 }
 
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.default.id
+  name    = local.api_domain
+  type    = "A"
+
+  alias {
+    name                   = aws_api_gateway_domain_name.api.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.api.cloudfront_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "api_certificate_validation" {
   zone_id = data.aws_route53_zone.default.zone_id
   name    = aws_acm_certificate.api.domain_validation_options.*.resource_record_name[0]
   type    = aws_acm_certificate.api.domain_validation_options.*.resource_record_type[0]
   records = [aws_acm_certificate.api.domain_validation_options.*.resource_record_value[0]]
   ttl     = 60
+}
+
+resource "aws_route53_record" "images" {
+  zone_id = data.aws_route53_zone.default.id
+  name    = local.images_domain
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.lgtms.domain_name
+    zone_id                = aws_cloudfront_distribution.lgtms.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
 
 resource "aws_route53_record" "images_certificate_validation" {
