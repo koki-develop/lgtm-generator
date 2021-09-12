bec46495b373000f0f706b4992341550722098b1
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-09-12 14:49:52 +0900

    sentry は prod 環境でのみ適用するようにした

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,15 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
+const config = {};
+
 const SentryWebpackPluginOptions = {
   silent: true,
 };
 
-module.exports = withSentryConfig({}, SentryWebpackPluginOptions);
+module.exports = (() => {
+  if (process.env.NEXT_PUBLIC_STAGE !== 'prod') {
+    return config;
+  }
+
+  return withSentryConfig(config, SentryWebpackPluginOptions);
+})();
