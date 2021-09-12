e52698fac2b12c2a373c6bfff52849f88ac57fb2
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-09-12 11:40:09 +0900

    [debug] エラーを手動で投げられるようにした

--- frontend/src/pageComponents/home/index.tsx
+++ frontend/src/pageComponents/home/index.tsx
@@ -8,13 +8,30 @@ import FavoritesPanel from './favoritesPanel';
 
 const Home: React.VFC = () => {
   const [tab, setTab] = useState<TabValue>(TabValue.lgtms);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const handleChangeTab = (value: TabValue) => {
     setTab(value);
   };
 
+  const handleChangeErrorMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setErrorMessage(e.currentTarget.value);
+  };
+
+  const handleClickError = () => {
+    throw new Error(errorMessage);
+  };
+
   return (
     <Layout>
+      <input
+        type="text"
+        value={errorMessage}
+        onChange={handleChangeErrorMessage}
+      />
+      <button onClick={handleClickError}>
+        error
+      </button>
       <Field>
         <Tabs
           value={tab}
