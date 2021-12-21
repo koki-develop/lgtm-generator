6b2b939f4833e1d63a0eb4bf473976ce185c47de
koki-develop <kou.pg.0131@gmail.com>
2021-12-21 15:59:59 +0900

    Field を修正

--- frontend/src/components/utils/Field.tsx
+++ frontend/src/components/utils/Field.tsx
@@ -1,13 +1,6 @@
 import { Box } from '@mui/material';
-import { Theme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 
-import withStyles from '@mui/styles/withStyles';
-import createStyles from '@mui/styles/createStyles';
-
-export default withStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      marginBottom: theme.spacing(2),
-    },
-  }),
-)(Box);
+export default styled(Box)(({ theme }) => ({
+  marginBottom: theme.spacing(2),
+}));
