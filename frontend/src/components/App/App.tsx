f0ac91c4d5a1bbdc1c17d24ac9ec68eb38893a9b
koki-develop <kou.pg.0131@gmail.com>
2021-12-21 14:58:42 +0900

    App コンポーネントを作成

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+
+const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const jssStyles = document.getElementById('jss-server-side');
+    if (jssStyles) jssStyles.remove();
+  }, []);
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_STAGE === 'prod') {
+      window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+        page_path: router.pathname,
+      });
+    }
+  }, [router.pathname]);
+
+  return <Component {...pageProps} />;
+};
+
+export default App;
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,25 +1,4 @@
-import React, { useEffect } from 'react';
-import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
+import App from '~/components/App';
 import '../styles/global.scss';
-
-const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const jssStyles = document.getElementById('jss-server-side');
-    if (jssStyles) jssStyles.remove();
-  }, []);
-
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_STAGE === 'prod') {
-      window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
-        page_path: router.pathname,
-      });
-    }
-  }, [router.pathname]);
-
-  return <Component {...pageProps} />;
-};
 
 export default App;
