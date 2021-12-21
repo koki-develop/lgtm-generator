e6353caea1c60010af6d6b3fc41d3adffe39399b
koki-develop <kou.pg.0131@gmail.com>
2021-12-22 08:42:23 +0900

    ExternalLink をリファクタ

--- frontend/src/components/utils/ExternalLink.tsx
+++ frontend/src/components/utils/ExternalLink.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import Link, { LinkProps } from '@mui/material/Link';
 
-type ExternalLinkProps = React.HTMLProps<HTMLAnchorElement>;
+type ExternalLinkProps = LinkProps;
 
-const ExternalLink: React.VFC<ExternalLinkProps> = (
-  props: ExternalLinkProps,
-) => {
-  return (
-    <a {...props} target='_blank' rel='noopener noreferrer'>
-      {props.children}
-    </a>
-  );
-};
+const ExternalLink: React.VFC<ExternalLinkProps> = React.memo(props => {
+  return <Link {...props} target='_blank' rel='noopener noreferrer' />;
+});
+
+ExternalLink.displayName = 'ExternalLink';
 
 export default ExternalLink;
