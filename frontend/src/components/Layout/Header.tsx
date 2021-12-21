b514248273cff44acf15e92c677364650a934310
koki-develop <kou.pg.0131@gmail.com>
2021-12-21 15:49:22 +0900

    Header で useStyles を使うのをやめた

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -2,32 +2,24 @@ import React from 'react';
 import Link from 'next/link';
 import { Routes } from '~/routes';
 import { AppBar, Toolbar, Typography } from '@mui/material';
-import { Theme } from '@mui/material/styles';
-
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    logo: {
-      fontFamily: 'ArchivoBlack',
-      fontSize: theme.typography.h4.fontSize,
-      [theme.breakpoints.down('sm')]: {
-        fontSize: theme.typography.h5.fontSize,
-      },
-    },
-  }),
-);
 
 const Header: React.VFC = () => {
-  const classes = useStyles();
-
   return (
     <AppBar color='primary' position='static'>
       <Toolbar>
         <Link href={Routes.home}>
           <a>
-            <Typography className={classes.logo}>LGTM Generator</Typography>
+            <Typography
+              sx={theme => ({
+                fontFamily: 'ArchivoBlack',
+                fontSize: theme => theme.typography.h4.fontSize,
+                [theme.breakpoints.down('sm')]: {
+                  fontSize: theme => theme.typography.h5.fontSize,
+                },
+              })}
+            >
+              LGTM Generator
+            </Typography>
           </a>
         </Link>
       </Toolbar>
