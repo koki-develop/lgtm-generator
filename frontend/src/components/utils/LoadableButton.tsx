de218e401841389a7840a075b2c93625d68bf6e9
koki-develop <kou.pg.0131@gmail.com>
2021-12-22 08:56:51 +0900

    LoadableButton をリファクタ

--- frontend/src/components/utils/LoadableButton.tsx
+++ frontend/src/components/utils/LoadableButton.tsx
@@ -1,34 +1,21 @@
 import React from 'react';
 import { Button, ButtonProps, CircularProgress } from '@mui/material';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 
 export type LoadableButtonProps = ButtonProps & {
   loading: boolean;
 };
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    circularProgress: {
-      position: 'absolute',
-    },
-  }),
-);
-
-const LoadableButton: React.VFC<LoadableButtonProps> = (
-  props: LoadableButtonProps,
-) => {
-  const classes = useStyles();
+const LoadableButton: React.VFC<LoadableButtonProps> = React.memo(props => {
   const { loading, children, ...buttonProps } = props;
 
   return (
     <Button {...buttonProps} disabled={props.disabled || loading}>
       {children}
-      {loading && (
-        <CircularProgress className={classes.circularProgress} size={24} />
-      )}
+      {loading && <CircularProgress size={24} sx={{ position: 'absolute' }} />}
     </Button>
   );
-};
+});
+
+LoadableButton.displayName = 'LoadableButton';
 
 export default LoadableButton;
