e1a9d7da32f3e56bec3c8446528594cc5bbf9677
koki-develop <kou.pg.0131@gmail.com>
2021-12-22 21:51:06 +0900

    Loading をリファクタ

--- frontend/src/components/utils/Loading.tsx
+++ frontend/src/components/utils/Loading.tsx
@@ -1,43 +1,37 @@
 import React from 'react';
 import { Box, CircularProgress, Typography } from '@mui/material';
-import { Theme } from '@mui/material/styles';
-
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-    },
-    circle: {
-      marginBottom: theme.spacing(1),
-    },
-    text: {
-      color: '#fff',
-      fontWeight: 'bold',
-    },
-  }),
-);
 
 type LoadingProps = {
   text?: string;
 };
 
-const Loading: React.VFC<LoadingProps> = (props: LoadingProps) => {
-  const classes = useStyles();
+const Loading: React.VFC<LoadingProps> = React.memo(props => {
+  const { text } = props;
 
   return (
-    <Box className={classes.root}>
-      <CircularProgress className={classes.circle} />
-      {props.text && (
-        <Typography className={classes.text}>{props.text}</Typography>
+    <Box
+      sx={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <CircularProgress sx={{ mb: 1 }} />
+      {text && (
+        <Typography
+          sx={{
+            color: '#ffffff',
+            fontWeight: 'bold',
+          }}
+        >
+          {text}
+        </Typography>
       )}
     </Box>
   );
-};
+});
+
+Loading.displayName = 'Loading';
 
 export default Loading;
