693f6000713f48c53b3e13c0bf32d762b9d0dc4c
koki-develop <kou.pg.0131@gmail.com>
2021-12-22 22:29:05 +0900

    UploadButton をリファクタ

--- frontend/src/components/pages/Home/UploadButton.tsx
+++ frontend/src/components/pages/Home/UploadButton.tsx
@@ -1,73 +1,56 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Fab } from '@mui/material';
-import { Theme } from '@mui/material/styles';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { AddCircle as AddCircleIcon } from '@mui/icons-material';
 import * as uuid from 'uuid';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    fab: {
-      bottom: theme.spacing(2),
-      fontWeight: 'bold',
-      position: 'fixed',
-      right: theme.spacing(2),
-      zIndex: 999,
-    },
-    input: {
-      display: 'none',
-    },
-    icon: {
-      marginRight: theme.spacing(1),
-    },
-  }),
-);
 
 type UploadButtonProps = {
   onChange: (file: File) => void;
 };
 
-const UploadButton: React.VFC<UploadButtonProps> = React.memo(
-  (props: UploadButtonProps) => {
-    const classes = useStyles();
+const UploadButton: React.VFC<UploadButtonProps> = React.memo(props => {
+  const inputFileRef = React.createRef<HTMLInputElement>();
+  const [inputFileKey, setInputFileKey] = useState<string>(uuid.v4());
 
-    const inputFileRef = React.createRef<HTMLInputElement>();
-    const [inputFileKey, setInputFileKey] = useState<string>(uuid.v4());
+  const handleClick = useCallback(() => {
+    inputFileRef.current?.click();
+  }, [inputFileRef]);
 
-    const handleClick = () => {
-      inputFileRef.current?.click();
-    };
+  const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputFileKey(uuid.v4());
+    const files = e.currentTarget.files;
+    if (files && files.length > 0) {
+      props.onChange(files[0]);
+    }
+  };
 
-    const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInputFileKey(uuid.v4());
-      const files = e.currentTarget.files;
-      if (files && files.length > 0) {
-        props.onChange(files[0]);
-      }
-    };
-
-    return (
-      <Fab
-        className={classes.fab}
-        color='primary'
-        variant='extended'
-        onClick={handleClick}
-      >
-        <input
-          className={classes.input}
-          key={inputFileKey}
-          ref={inputFileRef}
-          type='file'
-          accept={'image/*'}
-          onChange={handleChangeFile}
-        />
-        <AddCircleIcon className={classes.icon} />
-        アップロード
-      </Fab>
-    );
-  },
-);
+  return (
+    <Fab
+      color='primary'
+      variant='extended'
+      onClick={handleClick}
+      sx={{
+        bottom: theme => theme.spacing(2),
+        fontWeight: 'bold',
+        position: 'fixed',
+        right: theme => theme.spacing(2),
+        zIndex: 999,
+      }}
+    >
+      <input
+        key={inputFileKey}
+        ref={inputFileRef}
+        type='file'
+        accept={'image/*'}
+        onChange={handleChangeFile}
+        style={{
+          display: 'none',
+        }}
+      />
+      <AddCircleIcon sx={{ mr: 1 }} />
+      アップロード
+    </Fab>
+  );
+});
 
 UploadButton.displayName = 'UploadButton';
 
