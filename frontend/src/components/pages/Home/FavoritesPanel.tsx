f8c49217ad3530b23a02e015a8b121e664ddd4cb
koki-develop <kou.pg.0131@gmail.com>
2021-12-22 22:22:10 +0900

    FavoritesPanel をリファクタ

--- frontend/src/components/pages/Home/FavoritesPanel.tsx
+++ frontend/src/components/pages/Home/FavoritesPanel.tsx
@@ -1,49 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/material';
 import { useRecoilValue } from 'recoil';
 import { favoriteIdsState } from '~/recoil/atoms';
-import { Box, Typography } from '@mui/material';
-import { Theme } from '@mui/material/styles';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import LgtmCardList from '../../model/lgtm/LgtmCardList';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    emptyMessage: {
-      color: theme.palette.text.secondary,
-      textAlign: 'center',
-    },
-  }),
-);
 
 type FavoritesPanelProps = {
   show: boolean;
 };
 
-const FavoritesPanel: React.VFC<FavoritesPanelProps> = React.memo(
-  (props: FavoritesPanelProps) => {
-    const classes = useStyles();
+const FavoritesPanel: React.VFC<FavoritesPanelProps> = React.memo(props => {
+  const { show } = props;
 
-    const favoriteIds = useRecoilValue(favoriteIdsState);
-    const [showingFavoriteIds, setShowingFavoriteIds] = useState<string[]>([]);
+  const favoriteIds = useRecoilValue(favoriteIdsState);
+  const [showingFavoriteIds, setShowingFavoriteIds] = useState<string[]>([]);
 
-    useEffect(() => {
+  useEffect(() => {
+    if (!show) {
       setShowingFavoriteIds(favoriteIds);
-    }, [props.show]);
+    }
+  }, [favoriteIds, show]);
 
-    return (
-      <Box hidden={!props.show}>
-        {showingFavoriteIds.length === 0 ? (
-          <Typography className={classes.emptyMessage}>
-            お気に入りした LGTM 画像はありません。
-          </Typography>
-        ) : (
-          <LgtmCardList ids={showingFavoriteIds} />
-        )}
-      </Box>
-    );
-  },
-);
+  return (
+    <Box hidden={!show}>
+      {showingFavoriteIds.length === 0 ? (
+        <Typography
+          sx={{
+            color: theme => theme.palette.text.secondary,
+            textAlign: 'center',
+          }}
+        >
+          お気に入りした LGTM 画像はありません。
+        </Typography>
+      ) : (
+        <LgtmCardList ids={showingFavoriteIds} />
+      )}
+    </Box>
+  );
+});
 
 FavoritesPanel.displayName = 'FavoritesPanel';
 
