77759ad4d2ce102cfef4ade3167c19065cfca045
koki-develop <kou.pg.0131@gmail.com>
2021-12-22 22:07:06 +0900

    NotFound をリファクタ

--- frontend/src/components/pages/NotFound/NotFound.tsx
+++ frontend/src/components/pages/NotFound/NotFound.tsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import Layout from '~/components/Layout';
 import { Typography } from '@mui/material';
-import { Theme } from '@mui/material/styles';
 
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    message: {
-      fontSize: theme.typography.h5.fontSize,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  }),
-);
-
-const NotFound: React.VFC = () => {
-  const classes = useStyles();
-
+const NotFound: React.VFC = React.memo(() => {
   const message = 'お探しのページは見つかりませんでした';
 
   return (
     <Layout title={message}>
-      <Typography className={classes.message}>{message}</Typography>
+      <Typography
+        sx={{
+          fontSize: theme => theme.typography.h5.fontSize,
+          fontWeight: 'bold',
+          textAlign: 'center',
+        }}
+      >
+        {message}
+      </Typography>
     </Layout>
   );
-};
+});
+
+NotFound.displayName = 'NotFound';
 
 export default NotFound;
