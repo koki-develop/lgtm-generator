be9cdff376bf7e1b935e5ab6fd691f276a461125
koki-develop <kou.pg.0131@gmail.com>
2021-12-23 07:41:55 +0900

    ImageCard で useStyles を使うのをやめた

--- frontend/src/components/model/image/ImageCard.tsx
+++ frontend/src/components/model/image/ImageCard.tsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import { Image } from '~/types/image';
 import { Box, Card, CardActionArea, CardContent } from '@mui/material';
-import { Theme } from '@mui/material/styles';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { grey } from '@mui/material/colors';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    cardContent: {
-      padding: theme.spacing(1),
-      paddingBottom: `${theme.spacing(1)} !important`,
-    },
-    img: {
-      border: '1px solid',
-      borderColor: grey['A100'],
-      maxHeight: 140,
-      maxWidth: '100%',
-    },
-    imgContainer: {
-      alignItems: 'center',
-      display: 'flex',
-      height: 150,
-      justifyContent: 'center',
-    },
-  }),
-);
+import { Image } from '~/types/image';
 
 type ImageCardProps = {
   image: Image;
@@ -33,17 +9,27 @@ type ImageCardProps = {
 };
 
 const ImageCard: React.VFC<ImageCardProps> = (props: ImageCardProps) => {
-  const classes = useStyles();
-
   return (
     <Card>
       <CardActionArea onClick={props.onClick}>
-        <CardContent className={classes.cardContent}>
-          <Box className={classes.imgContainer}>
+        <CardContent sx={{ p: 1 }}>
+          <Box
+            sx={{
+              alignItems: 'center',
+              display: 'flex',
+              height: 150,
+              justifyContent: 'center',
+            }}
+          >
             <img
-              className={classes.img}
               src={props.image.url}
               alt={props.image.title}
+              style={{
+                border: '1px solid',
+                borderColor: grey['A100'],
+                maxHeight: 140,
+                maxWidth: '100%',
+              }}
             />
           </Box>
         </CardContent>
