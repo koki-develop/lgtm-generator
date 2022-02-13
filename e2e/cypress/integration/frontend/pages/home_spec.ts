940d46a09801ec4f80617318c14949ad8a2b4ce7
koki-develop <kou.pg.0131@gmail.com>
2022-02-13 21:42:08 +0900

    画像検索タブ・お気に入りタブのテストをリファクタ

--- e2e/cypress/integration/frontend/pages/home_spec.ts
+++ e2e/cypress/integration/frontend/pages/home_spec.ts
@@ -111,93 +111,63 @@ describe("/", () => {
       });
 
       describe("画像検索タブ", () => {
-        it("正しく動作すること");
+        before(() => {
+          cy.getByTestId("home-tab-search-images").click();
+          cy.search().should("equal", "?tab=search_images");
+        });
+
+        describe("LGTM 画像生成", () => {
+          beforeEach(() => {
+            cy.getByTestId("search-images-keyword-input")
+              .clear()
+              .type(randomSearchKeyword())
+              .enter();
+            cy.getByTestId("image-card-action-area").first().click();
+            cy.getByTestId("lgtm-form-generate-button").click();
+          });
+          it("LGTM 画像を生成した旨を表示すること", () => {
+            cy.contains(
+              {
+                ja: "LGTM 画像を生成しました",
+                en: "Generated LGTM image.",
+              }[locale]
+            );
+          });
+        });
       });
 
       describe("お気に入りタブ", () => {
-        it("正しく動作すること");
+        before(() => {
+          cy.getByTestId("home-tab-favorites").click();
+          cy.search().should("equal", "?tab=favorites");
+        });
+
+        describe("お気に入り追加・解除", () => {
+          it("正しく動作すること", () => {
+            cy.getByTestId("no-favorites-text").first().contains(
+              {
+                ja: "お気に入りした LGTM 画像はありません。",
+                en: "There are no favorites yet.",
+              }[locale]
+            );
+            // お気に入りを追加
+            cy.getByTestId("home-tab-lgtms").click();
+            cy.getByTestId("lgtm-card-favorite-button").first().click();
+            cy.getByTestId("home-tab-favorites").click();
+            cy.getByTestId("no-favorites-text").should("not.exist");
+
+            // お気に入りを解除
+            cy.getByTestId("lgtm-card-unfavorite-button")
+              .visible()
+              .first()
+              .click();
+            cy.getByTestId("no-favorites-text").should("not.exist"); // 解除直後はまだメッセージが表示されないことを検証
+            cy.getByTestId("home-tab-lgtms").click();
+            cy.getByTestId("home-tab-favorites").click();
+            cy.getByTestId("no-favorites-text").should("exist");
+          });
+        });
       });
     });
   }
-
-  // for (const locale of locales) {
-  //   it("works", () => {
-  //     const pathname = locale === "ja" ? "/" : `/${locale}`;
-
-  //     cy.getByTestId("translate-open-button").click();
-  //     cy.getByTestId(`translate-list-item-${locale}`).click();
-  //     cy.pathname().should("equal", pathname);
-
-  //     // 画像検索
-  //     cy.getByTestId("home-tab-search-images").click();
-  //     cy.pathname().should("equal", pathname);
-  //     cy.search().should("equal", "?tab=search_images");
-  //     cy.getByTestId("search-images-keyword-input")
-  //       .clear()
-  //       .type(randomSearchKeyword())
-  //       .enter();
-  //     cy.getByTestId("image-card-action-area").first().click();
-  //     cy.getByTestId("lgtm-form-generate-button").click();
-  //     cy.contains(
-  //       {
-  //         ja: "LGTM 画像を生成しました",
-  //         en: "Generated LGTM image.",
-  //       }[locale]
-  //     );
-
-  //     // お気に入り
-  //     cy.getByTestId("home-tab-favorites").click();
-  //     cy.pathname().should("equal", pathname);
-  //     cy.search().should("equal", "?tab=favorites");
-  //     cy.getByTestId("no-favorites-text").first().contains(
-  //       {
-  //         ja: "お気に入りした LGTM 画像はありません。",
-  //         en: "There are no favorites yet.",
-  //       }[locale]
-  //     );
-  //     cy.getByTestId("home-tab-lgtms").click();
-  //     cy.getByTestId("lgtm-card-favorite-button").first().click();
-  //     cy.getByTestId("home-tab-favorites").click();
-  //     cy.getByTestId("no-favorites-text").should("not.exist");
-  //     cy.getByTestId("lgtm-card-unfavorite-button").visible().first().click();
-  //     cy.getByTestId("no-favorites-text").should("not.exist");
-  //     cy.getByTestId("home-tab-lgtms").click();
-  //     cy.getByTestId("home-tab-favorites").click();
-  //     cy.getByTestId("no-favorites-text").should("exist");
-
-  //     // LGTM
-  //     cy.getByTestId("home-tab-lgtms").click();
-  //     cy.pathname().should("equal", pathname);
-  //     cy.search().should("equal", "");
-  //     cy.getByTestId("upload-file-input").attachFile("images/gray.png");
-  //     cy.getByTestId("lgtm-form-generate-button").click();
-  //     cy.contains(
-  //       {
-  //         ja: "LGTM 画像を生成しました",
-  //         en: "Generated LGTM image.",
-  //       }[locale]
-  //     );
-  //     cy.getByTestId("lgtm-card-copy-button").first().click();
-  //     cy.window().then((window) => {
-  //       cy.stub(window, "prompt").returns("DISABLED WINDOW PROMPT");
-  //     });
-  //     cy.getByTestId("lgtm-card-copy-html-button").click();
-  //     cy.contains(
-  //       {
-  //         ja: "クリップボードにコピーしました",
-  //         en: "Copied to clipboard",
-  //       }[locale]
-  //     );
-  //     cy.getByTestId("lgtm-card-report-button").first().click();
-  //     cy.getByTestId("report-form-type-radio-other").click();
-  //     cy.getByTestId("report-form-text-input").type("e2e test");
-  //     cy.getByTestId("report-form-send-button").click();
-  //     cy.contains(
-  //       {
-  //         ja: "送信しました",
-  //         en: "Sent.",
-  //       }[locale]
-  //     );
-  //   });
-  // }
 });
