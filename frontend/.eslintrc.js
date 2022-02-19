e11935b2486ffc695408f0d853db77fc772345b6
koki-develop <kou.pg.0131@gmail.com>
2022-02-19 15:42:16 +0900

    import 関連の設定を追加

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
     'prettier',
   ],
   parserOptions: {
@@ -22,7 +23,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'import', 'unused-imports'],
   rules: {
     'react/prop-types': 'off',
     semi: ['error', 'always'],
@@ -30,10 +31,78 @@ module.exports = {
     quotes: ['error', 'single'],
     'object-curly-spacing': ['error', 'always'],
     'react/jsx-tag-spacing': ['error'],
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'object',
+          'type',
+          'index',
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          {
+            pattern: '@/components/App/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/Layout/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/pages/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/providers/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/model/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/utils/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/hooks/**',
+            group: 'internal',
+            position: 'before',
+          },
+          { pattern: '@/lib/**', group: 'internal', position: 'before' },
+          { pattern: '@/recoil/**', group: 'internal', position: 'before' },
+          { pattern: '@/types/**', group: 'internal', position: 'before' },
+          { pattern: '@/locales/**', group: 'internal', position: 'before' },
+          { pattern: '@/routes', group: 'internal', position: 'before' },
+          { pattern: '@/styles/**', group: 'internal', position: 'before' },
+        ],
+      },
+    ],
   },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            alwaysTryTypes: true,
+            project: './',
+          },
+        },
+      },
       extends: ['plugin:@typescript-eslint/recommended'],
       plugins: ['@typescript-eslint'],
       parser: '@typescript-eslint/parser',
