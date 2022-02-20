d754e3b9ae686933b53f8da37138b8137982f1b6
koki-develop <kou.pg.0131@gmail.com>
2022-02-20 12:25:34 +0900

    広告を設置

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
+import Box from '@mui/material/Box';
 import { useRouter } from 'next/router';
 import React, { useCallback, useMemo } from 'react';
 import Layout from '~/components/Layout';
+import Admax from '~/components/utils/Admax';
 import Field from '~/components/utils/Field';
 import FavoritesPanel from './FavoritesPanel';
 import LgtmsPanel from './LgtmsPanel';
@@ -25,6 +27,10 @@ const Home: React.VFC = React.memo(() => {
 
   return (
     <Layout>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+        <Admax admaxId={process.env.NEXT_PUBLIC_ADMAX_ID} type='switch' />
+      </Box>
+
       <Field>
         <Tabs value={tab} onChange={handleChangeTab} />
       </Field>
