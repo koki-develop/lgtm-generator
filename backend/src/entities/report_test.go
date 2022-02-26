6f89b0f9148cf300e4f901074913fdb3ae2ef058
koki-develop <kou.pg.0131@gmail.com>
2022-02-26 15:04:43 +0900

    ReportCreateInput のテストを実装

--- backend/src/entities/report_test.go
+++ backend/src/entities/report_test.go
@@ -1,33 +1,125 @@
 package entities
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func buildReportCreateInput() *ReportCreateInput {
+	return &ReportCreateInput{
+		LGTMID: "00000000-0000-0000-0000-000000000000",
+		Type:   ReportTypeOther,
+		Text:   "hello world",
+	}
+}
+
+func Test_buildReportCreateInput(t *testing.T) {
+	t.Run("buildCreateReportInput return valid input", func(t *testing.T) {
+		ipt := buildReportCreateInput()
+		assert.Nil(t, ipt.Valid())
+	})
+}
+
 func TestReportType_Valid(t *testing.T) {
-	t.Run("return nil when valid report type", func(t *testing.T) {
-		typs := []ReportType{
-			"illegal",
-			"inappropriate",
-			"other",
+	testcases := []struct {
+		value ReportType
+		valid bool
+	}{
+		{"illegal", true},
+		{"inappropriate", true},
+		{"other", true},
+		{"invalid", false},
+		{"hoge", false},
+		{"fuga", false},
+	}
+	for _, testcase := range testcases {
+		if testcase.valid {
+			assert.NoError(t, testcase.value.Valid())
+		} else {
+			assert.NotNil(t, testcase.value.Valid())
 		}
-		for _, tp := range typs {
-			assert.Nil(t, tp.Valid())
+	}
+}
+
+func TestReportCreateInput_Valid(t *testing.T) {
+	t.Run("LGTMID", func(t *testing.T) {
+		testcases := []struct {
+			value string
+			valid bool
+		}{
+			{"00000000-0000-0000-0000-000000000000", true},
+			{"aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa", true},
+			{"", false},
+			{" ", false},
+			{"      ", false},
+			{"invalid_id", false},
+			{"AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA", false},
+		}
+		for _, testcase := range testcases {
+			ipt := buildReportCreateInput()
+			ipt.LGTMID = testcase.value
+			err := ipt.Valid()
+			if testcase.valid {
+				assert.NoError(t, err)
+			} else {
+				assert.NotNil(t, err)
+			}
 		}
 	})
-	t.Run("return error when invalid report type", func(t *testing.T) {
-		typs := []ReportType{
-			"invalid",
-			"hoge",
-			"fuga",
+
+	t.Run("Type", func(t *testing.T) {
+		testcases := []struct {
+			value ReportType
+			valid bool
+		}{
+			{"illegal", true},
+			{"inappropriate", true},
+			{"other", true},
+			{"", false},
+			{"invalid type", false},
 		}
-		for _, tp := range typs {
-			err := tp.Valid()
-			assert.NotNil(t, err)
-			assert.EqualError(t, err, fmt.Sprintf("invalid report type: %s", tp))
+		for _, testcase := range testcases {
+			ipt := buildReportCreateInput()
+			ipt.Type = testcase.value
+			err := ipt.Valid()
+			if testcase.valid {
+				assert.NoError(t, err)
+			} else {
+				assert.NotNil(t, err)
+			}
+		}
+	})
+
+	t.Run("Text", func(t *testing.T) {
+		testcases := []struct {
+			value string
+			valid bool
+		}{
+			{"", true},
+			{strings.Repeat("a", 100), true},
+			{strings.Repeat("a", 999), true},
+			{strings.Repeat("a", 1000), true},
+			{strings.Repeat("あ", 100), true},
+			{strings.Repeat("あ", 999), true},
+			{strings.Repeat("あ", 1000), true},
+			{strings.Repeat("a", 1001), false},
+			{strings.Repeat("a", 1002), false},
+			{strings.Repeat("a", 2000), false},
+			{strings.Repeat("あ", 1001), false},
+			{strings.Repeat("あ", 1002), false},
+			{strings.Repeat("あ", 2000), false},
+		}
+		for _, testcase := range testcases {
+			ipt := buildReportCreateInput()
+			ipt.Text = testcase.value
+			err := ipt.Valid()
+			if testcase.valid {
+				assert.NoError(t, err)
+			} else {
+				assert.NotNil(t, err)
+			}
 		}
 	})
 }
