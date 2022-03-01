1b579e379743c4fa3e652e94a9402b232228402a
koki-develop <kou.pg.0131@gmail.com>
2022-03-01 16:47:09 +0900

    LGTMsUsecase.Delete() のテストを実装

--- backend/src/usecases/lgtms_usecase_test.go
+++ backend/src/usecases/lgtms_usecase_test.go
@@ -4,12 +4,18 @@ import (
 	"testing"
 
 	mocks "github.com/koki-develop/lgtm-generator/backend/mocks/src/adapters/gateways/iface"
+	"github.com/koki-develop/lgtm-generator/backend/src/entities"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
 type mocksForLGTMsUsecase struct {
 	lgtmsUsecase    *LGTMsUsecase
 	lgtmsRepository *mocks.LGTMsRepository
+}
+
+func (ms *mocksForLGTMsUsecase) AssertExpectations(t *testing.T) {
+	ms.lgtmsRepository.AssertExpectations(t)
 }
 
 func buildMocksForLGTMsUsecase() *mocksForLGTMsUsecase {
@@ -44,5 +50,28 @@ func TestLGTMsUsecase_Create(t *testing.T) {
 }
 
 func TestLGTMsUsecase_Delete(t *testing.T) {
-	t.Skip()
+	t.Run("return nil", func(t *testing.T) {
+		ipt := &entities.LGTMDeleteInput{ID: "00000000-0000-0000-0000-000000000000"}
+
+		ms := buildMocksForLGTMsUsecase()
+		ms.lgtmsRepository.On("Delete", ipt.ID).Return(nil)
+		uc := ms.lgtmsUsecase
+
+		err := uc.Delete(ipt)
+
+		assert.NoError(t, err)
+		ms.AssertExpectations(t)
+	})
+	t.Run("return error when failed to delete", func(t *testing.T) {
+		ipt := &entities.LGTMDeleteInput{ID: "00000000-0000-0000-0000-000000000000"}
+
+		ms := buildMocksForLGTMsUsecase()
+		ms.lgtmsRepository.On("Delete", ipt.ID).Return(errors.New("SOMETHING_WRONG"))
+		uc := ms.lgtmsUsecase
+
+		err := uc.Delete(ipt)
+
+		assert.EqualError(t, err, "SOMETHING_WRONG")
+		ms.AssertExpectations(t)
+	})
 }
