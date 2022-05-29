537fe15392277e39e56e06acace00b5a2932b565
koki-develop <kou.pg.0131@gmail.com>
2022-05-29 21:12:32 +0900

    ListKeys を実装

--- backend/src/infrastructures/iface/file_storage.go
+++ backend/src/infrastructures/iface/file_storage.go
@@ -2,6 +2,7 @@ package iface
 
 type FileStorage interface {
 	Save(path, contentType string, data []byte) error
+	ListKeys() ([]string, error)
 	IssueSignedURL(key string) (string, error)
 	Delete(key string) error
 }
