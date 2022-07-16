59ae74601134743b9e24cc5fd7ccb1e6ba34786f
koki-develop <kou.pg.0131@gmail.com>
2022-07-16 11:23:29 +0900

    circleci 用の iam ポリシーを作成

--- terraform/circleci/iam.tf
+++ terraform/circleci/iam.tf
@@ -1,3 +1,28 @@
 data "aws_iam_openid_connect_provider" "circleci" {
   url = "https://oidc.circleci.com/org/${var.circleci_organization_id}"
 }
+
+resource "aws_iam_role" "circleci" {
+  name               = "${local.prefix}-role"
+  assume_role_policy = data.aws_iam_policy_document.circleci_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "circleci_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.circleci.arn]
+    }
+  }
+}
+
+data "aws_iam_policy" "administrator_access" {
+  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "circleci-administrator_access" {
+  role       = aws_iam_role.circleci.name
+  policy_arn = data.aws_iam_policy.administrator_access.arn
+}
