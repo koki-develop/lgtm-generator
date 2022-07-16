291636f3c82ae169dcc563b0f3ca73b5998ee723
koki-develop <kou.pg.0131@gmail.com>
2022-07-16 16:28:38 +0900

    画像用の S3 の暗号化・パブリックアクセスブロックを有効化

--- terraform/app/modules/aws/s3.tf
+++ terraform/app/modules/aws/s3.tf
@@ -22,3 +22,22 @@ data "aws_iam_policy_document" "images_bucket_policy" {
     resources = ["${aws_s3_bucket.images.arn}/*"]
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
