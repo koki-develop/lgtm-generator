8280df07c4c6200ec0acbc129ef32c3b684e9ae9
koki-develop <kou.pg.0131@gmail.com>
2022-07-16 09:43:06 +0900

    バケットポリシーを import

--- terraform/modules/aws/s3.tf
+++ terraform/modules/aws/s3.tf
@@ -6,3 +6,19 @@ resource "aws_s3_bucket" "images" {
     Name = "${local.prefix_backend}-images"
   }
 }
+
+resource "aws_s3_bucket_policy" "images" {
+  bucket = aws_s3_bucket.images.id
+  policy = data.aws_iam_policy_document.images_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "images_bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.images.iam_arn]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.images.arn}/*"]
+  }
+}
