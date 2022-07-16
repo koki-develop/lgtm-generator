7c9fa55946e2a75aeb0e4410ff47eea7e3931320
koki-develop <kou.pg.0131@gmail.com>
2022-07-16 14:49:25 +0900

    circleci project id の検証を追加

--- terraform/circleci/iam.tf
+++ terraform/circleci/iam.tf
@@ -11,6 +11,11 @@ data "aws_iam_policy_document" "circleci_assume_role_policy" {
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringLike"
+      variable = "oidc.circleci.com/org/${var.circleci_organization_id}:sub"
+      values   = ["org/${var.circleci_organization_id}/project/${var.circleci_project_id}/user/*"]
+    }
     principals {
       type        = "Federated"
       identifiers = [data.aws_iam_openid_connect_provider.circleci.arn]
--- terraform/circleci/variables.tf
+++ terraform/circleci/variables.tf
@@ -2,3 +2,8 @@ variable "circleci_organization_id" {
   type      = string
   sensitive = true
 }
+
+variable "circleci_project_id" {
+  type      = string
+  sensitive = true
+}
