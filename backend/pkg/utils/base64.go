8e85206d630ae86222f9fbfc7a6627b9ae03f2da
koki-develop <kou.pg.0131@gmail.com>
2022-07-17 10:09:59 +0900

    utils.Base64Decode() を実装

--- backend/pkg/utils/base64.go
+++ backend/pkg/utils/base64.go
@@ -1,8 +1,20 @@
 package utils
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+
+	"github.com/pkg/errors"
+)
 
 func IsBase64(str string) bool {
 	_, err := base64.StdEncoding.DecodeString(str)
 	return err == nil
+}
+
+func Base64Decode(str string) ([]byte, error) {
+	b, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return b, nil
 }
