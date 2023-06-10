645ab6c61e4c1d2d9abcf08e4209c4c6f6441466
koki-develop <kou.pg.0131@gmail.com>
2023-06-10 14:48:34 +0900

    createEmotionCache を修正

--- frontend/src/lib/emotion.ts
+++ frontend/src/lib/emotion.ts
@@ -1,5 +1,15 @@
-import createCache, { EmotionCache } from '@emotion/cache';
+import createCache, { EmotionCache, Options } from '@emotion/cache';
+
+const isBrowser = typeof document !== 'undefined';
 
 export const createEmotionCache = (): EmotionCache => {
-  return createCache({ key: 'css', prepend: true });
+  const options: Options = { key: 'mui-style' };
+  if (isBrowser) {
+    const emotionInsertionPoint = document.querySelector<HTMLMetaElement>(
+      'meta[name="emotion-insertion-point"]',
+    );
+    options.insertionPoint = emotionInsertionPoint ?? undefined;
+  }
+
+  return createCache(options);
 };
